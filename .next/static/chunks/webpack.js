c5a4b50e03929a8d506e7511dd7c5dd8fa3add75
Guilherme Vieira <guilherme.vieira@agetec.campogrande.ms.gov.br>
2023-11-14 13:37:28 -0400

    :fix: correçãoe stilização!

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6275b9a0e91f6604"; }
+/******/ 		__webpack_require__.h = function() { return "95da83f27d929098"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
